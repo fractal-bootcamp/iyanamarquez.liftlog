b3c42450d838918ea80d24daf8139e4899db4180
Iyana <114933494+yayakixtenet@users.noreply.github.com>
2024-08-19 10:52:33 -0400

    Initial Commit

---
+++ LiftLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiftLog
+//
+//  Created by Iyana Marquez on 8/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiftLog/LiftLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftLogApp.swift
+//  LiftLog
+//
+//  Created by Iyana Marquez on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LiftLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
